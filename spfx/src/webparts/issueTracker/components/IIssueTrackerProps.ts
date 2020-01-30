0e18537d8012712950c245cdb544455a97ca3e74
william <william@umaknow.com>
2020-01-30 15:45:40 -0500

    add spfx solution

---
+++ spfx/src/webparts/issueTracker/components/IIssueTrackerProps.ts
@@ -0,0 +1,3 @@
+export interface IIssueTrackerProps {
+  description: string;
+}
---
+++ spfx/src/webparts/issueTracker/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IIssueTrackerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'IssueTrackerWebPartStrings' {
+  const strings: IIssueTrackerWebPartStrings;
+  export = strings;
+}
