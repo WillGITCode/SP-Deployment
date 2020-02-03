e5d46c3cca0cb4645c4853ad0ef7a7553ec9de7c
william <william@umaknow.com>
2020-02-03 15:22:13 -0500

    test build + deploy

--- spfx/src/webparts/issueTracker/components/IssueTracker.tsx
+++ spfx/src/webparts/issueTracker/components/IssueTracker.tsx
@@ -1,20 +1,25 @@
-import * as React from 'react';
-import styles from './IssueTracker.module.scss';
-import { IIssueTrackerProps } from './IIssueTrackerProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import styles from "./IssueTracker.module.scss";
+import { IIssueTrackerProps } from "./IIssueTrackerProps";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-export default class IssueTracker extends React.Component<IIssueTrackerProps, {}> {
+export default class IssueTracker extends React.Component<
+  IIssueTrackerProps,
+  {}
+> {
   public render(): React.ReactElement<IIssueTrackerProps> {
     return (
-      <div className={ styles.issueTracker }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
+      <div className={styles.issueTracker}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <span className={styles.title}>Welcome to SharePoint!</span>
+              <p className={styles.subTitle}>Hey, hey, hey!</p>
+              <p className={styles.description}>
+                {escape(this.props.description)}
+              </p>
+              <a href="https://aka.ms/spfx" className={styles.button}>
+                <span className={styles.label}>Learn Everything</span>
               </a>
             </div>
           </div>
